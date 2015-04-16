004c3b37eb52731cb77d2d8497de73eb089ebba7
Mihai Veres <veres85@yahoo.com>
2015-04-17 00:01:15 +0300

    functions createGrid, openSite and isOpen
    - partial implementation for openSite
    - unit test

---
+++ Percolation/percolation.fs
@@ -0,0 +1,30 @@
+﻿module percolation
+
+open System
+
+type Site = Closed | Open | Full
+type Grid = Site [,]
+
+let createGrid n =
+    if n <= 0 then raise (ArgumentException())
+    Array2D.create n n Closed
+
+
+let validate i j grid =
+    let n = Array2D.length1 grid
+    let m = Array2D.length2 grid
+    if m <> n then raise (ArgumentException("Percolation grid must be square"))
+    if i < 0 || j < 0 || i >= n || j >= n then raise (ArgumentOutOfRangeException())
+
+
+let openSite i j grid =
+    validate i j grid
+    Array2D.set grid i j Open
+    grid
+
+
+let isOpen i j grid =
+    validate i j grid
+    grid.[i,j] = Open
+
+
---
+++ Percolation/tests.fs
@@ -0,0 +1,46 @@
+﻿module tests
+
+open NUnit.Framework
+open FsUnit
+open percolation
+open System
+
+
+[<Test; ExpectedException(typeof<ArgumentException>)>]
+let ``createGrid with n <= 0 throws invalid argument`` () =
+    createGrid -1 |> ignore
+
+
+[<TestCase(1)>]
+[<TestCase(13)>]
+let ``createGrid n creates a nxn grid with closed sites`` n =
+    let expectedGrid = Array2D.create n n Closed
+    createGrid n |> should equal expectedGrid
+
+
+[<Test; ExpectedException(typeof<ArgumentOutOfRangeException>)>]
+let ``openSite params should be >= 0`` () =
+    let grid = createGrid 3
+    openSite -1 2 grid |> ignore
+
+
+[<Test; ExpectedException(typeof<ArgumentOutOfRangeException>)>]
+let ``openSite params should be < n`` () =
+    let grid = createGrid 3
+    openSite 0 3 grid |> ignore
+
+
+[<Test>]
+let ``openSite i,j should open only the [i,j] site `` () =
+    let initialGrid = createGrid 2
+    let expectedGrid = createGrid 2
+    Array2D.set expectedGrid 1 1 Open
+    openSite 1 1 initialGrid |> should equal expectedGrid
+
+
+[<Test>]
+let ``isOpen should work`` () =
+    let grid = openSite 1 1 (createGrid 2)
+    isOpen 1 1 grid |> should be True
+    isOpen 0 0 grid |> should be False
+
