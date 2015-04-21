2b7222cf7f5e0eb8f6f79109f297249f70f4827a
Mihai Veres <mihai.veres@ullink.com>
2015-04-21 18:11:18 +0300

    isOpen, isFull functions and some cleanup.
    Added VS project that links to .fs files
    
    Change-Id: Ibcbda33906a8ef408ffb2c21836026649176200e

--- Percolation/percolation.fs
+++ Percolation/percolation.fs
@@ -5,21 +5,47 @@ open System
 type Site = Closed | Open | Full
 type PercolationModel = array<int*Site> * int
 
+
 let createModel n : PercolationModel =
     let last = n*n+1
     let ids = [|0..last|] |> Array.map (fun i -> (i,Closed))
-    Array.set ids 0 (0,Open)
-    Array.set ids last (last,Open)
+    Array.set ids 0 (0, Full)
+    Array.set ids last (last, Open)
     ids,n
 
-let validate i j n =
+
+let validate i j model =
+    let n = snd model
     if i <= 0 || j <= 0 || i > n || j > n then raise (new ArgumentException())
 
-let openSite i j model =
-    let n = snd model
-    validate i j n
-    let ids = fst model
-    let index = (i-1)*n + j
-    Array.set ids index (index, Open)
-    ids,n
 
+let getIndex i j model =
+        let n = snd model
+        (i-1)*n + j
+
+
+let openSite i j model =
+    validate i j model
+
+    let index = getIndex i j model
+    let ids = fst model
+
+    Array.set ids index (index, Open)
+    ids, (snd model)
+
+
+let siteMatches expectedSite i j (model: PercolationModel) =
+    validate i j model
+
+    let index = getIndex i j model
+    let ids = fst model
+
+    ids.[index] |> snd |> (=) expectedSite
+
+
+let isOpen i j model =
+    siteMatches Open i j model
+
+
+let isFull i j model =
+    siteMatches Full i j model
--- Percolation/tests.fs
+++ Percolation/tests.fs
@@ -8,7 +8,7 @@ open System
 
 [<Test>]
 let ``Create 2 x 2 percolation grid creates 0,,5 id array`` ()=
-    let expected = [|(0, Open); (1, Closed); (2, Closed); (3, Closed); (4, Closed); (5, Open)|]
+    let expected = [|(0, Full); (1, Closed); (2, Closed); (3, Closed); (4, Closed); (5, Open)|]
     let model = createModel 2
     model |> fst |> should equal expected
     model |> snd |> should equal 2
@@ -25,10 +25,11 @@ let ``Given invalid params When open is called Then it throws argument exception
 
 [<Test>]
 let ``Given 2 x 2 percolation grid When opening (2,1) Then (2,1) is open`` () =
-    let expected = [|(0, Open); (1, Closed); (2, Closed); (3, Open); (4, Closed); (5, Open)|]
-    openSite 2 1 (createModel 2) |> snd |> should equal expected
+    let model = openSite 2 1 (createModel 2)
+    isOpen 2 1 model |> should be True
+
 
 [<Test>]
-let ``Given 2 x 2 percolation grid When opening (1,2) Then (1,2) is open`` () =
-    let expected = [|(0, Open); (1, Closed); (2, Open); (3, Closed); (4, Closed); (5, Open)|]
-    (openSite 2 1 (createModel 2) |> fst).[2] |> snd |>  should equal Open
+let ``Given 2 x 2 percolation grid When opening (1,2) Then (1,2) is full`` () =
+    let model = openSite 1 2 (createModel 2)
+    isFull 1 2 model |>  should be True
